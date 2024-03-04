52d5ef8faccbdda816b17307e86a423ea2398684
me_quah <alfisahdamayanti135@gmail.com>
2024-03-04 22:38:34 +0700

    intent

--- app/src/main/java/com/example/project_1/MainActivity.kt
+++ app/src/main/java/com/example/project_1/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.project_1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,55 +9,23 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var edtWidth: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var edtLength: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
-
-    companion object {
-        private const val STATE_RESULT = "state_result"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        edtLength = findViewById(R.id.edt_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
-        btnCalculate.setOnClickListener(this)
 
-        if (savedInstanceState != null) {
-            val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text=result
-        }
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
 
     }
 
     override fun onClick(view: View?) {
-        if (view?.id == R.id.btn_calculate) {
-            val inputLength = edtLength.text.toString().trim()
-            val inputWidth = edtWidth.text.toString().trim()
-            val inputHeight = edtHeight.text.toString().trim()
-            var isEmptyFields = false
-            if (inputLength.isEmpty()) {
-                isEmptyFields = true
-                edtLength.error = "Field tidak boleh kosong"
-            }
-            if (inputWidth.isEmpty()) {
-                isEmptyFields = true
-                edtWidth.error = "Field tidak boleh kosong"
-            }
-            if (inputHeight.isEmpty()) {
-                isEmptyFields = true
-                edtHeight.error = "Field tidak boleh kosong"
-            }
-            if (!isEmptyFields) {
-                val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-                tvResult.text = volume.toString()
+        when (view?.id) {
+            R.id.btn_move_activity -> {
+            val  moveIntent = Intent(this@MainActivity, MainActivity2::class.java)
+                startActivities(arrayOf(moveIntent))
             }
         }
+
+           }
     }
-}
